990a58e42b26fc68f4aed1d5f447c6183f89ac62
laetitiamichel <laetitia.michel@devoteam.com>
2024-02-01 16:57:41 +0100

    PHP

--- pages/head.inc.php
+++ pages/head.inc.php
@@ -1,7 +1,5 @@
-<?php
-
 <!DOCTYPE html>
-<html lang="<? $_config->_lang[0]?>">
+<html lang="<?= Page::$_lang ?>">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,11 +8,11 @@
     <link rel="apple-touch-icon" sizes="180x180" href="./favicone/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./favicone/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./favicone/favicon-16x16.png">
-    <link rel="manifest" href="./favicone/site.webmanifest">
+    <link rel="manifest" href="<?= Page::$_manifest ?>">
 
-    <link rel="stylesheet" href="./css/formulaire_connexion.css">
-    <link rel="stylesheet" href="./css/formulaire_page_1.css">
-    <link rel="stylesheet" href="./css/header.css">
-    <link rel="stylesheet" href="./css/reset.css">
-    <title><?= title ?></title>
+    <link rel="stylesheet" href="<?= $_page->_link_formulaire_connexion ?>">
+    <link rel="stylesheet" href="<?= $_page->_link_formulaire_page_1 ?>">
+    <link rel="stylesheet" href="<?= $_page->_link_header ?>">
+    <link rel="stylesheet" href="<?= $_page->_link_reset ?>">
+    <title><?= $_page->_title ?></title>
 </head>
