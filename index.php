e14c092075af6550c0b52c964ac7a5e28e42a619
laetitiamichel <laetitia.michel@devoteam.com>
2024-02-01 17:10:51 +0100

    maj

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-
     include_once __DIR__ ."/pages/head.inc.php";
-
     include_once __DIR__ ."/pages/creerUnCompte.inc.php";
-
 ?>
-# inclusion du head
-# inclusion du formulaire créer un compte:
